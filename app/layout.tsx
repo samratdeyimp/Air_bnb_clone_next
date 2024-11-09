b22b907c23859bb530a81754eed1e3e0d149b49c
Samrat Dey <samratdeyimp@gmail.com>
2024-11-09 18:43:14 +0530

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <RegisterModal/>
+          <Navbar />
+        </ClientOnly>
+
+        {children}
+      </body>
+    </html>
+  );
+}
